6f28c5e6a56944e2bc349fb6f9f02cc42bdb9875
Mark Gami <159137725+Mark-0731@users.noreply.github.com>
2024-10-12 10:51:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose
 
 app.use(
   cors({
-    origin: ["https://mark-react-ten.vercel.app","http://localhost:5173"]
+    origin: ["https://mark-react-ten.vercel.app","http://localhost:5173"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
